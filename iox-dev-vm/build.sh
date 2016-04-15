6abad3b18cffd06387c44a3d82811942e373c85e
Nathan Sowatskey <nathan@nathan.to>
2016-04-15 15:30:35 +0100

    Constantly striving ...

--- iox-dev-vm/build.sh
+++ iox-dev-vm/build.sh
@@ -1,5 +1,7 @@
-rm -rf *.box ~/.vagrant.d/boxes/iox-dev-vm-virtualbox/; \
-rm -rf *.box ~/.vagrant.d/boxes/iox-dev-vm-vmware/; \
+rm -rf ~/.vagrant.d/boxes/iox-dev-vm-virtualbox/; \
+rm -rf ~/.vagrant.d/boxes/iox-dev-vm-vmware/; \
+rm -rf builds/*.box; \
 packer build -force ubuntu1404.json;\
 vagrant box add --force --name "iox-dev-vm-virtualbox" builds/virtualbox-ubuntu1404.box;\
-vagrant box add --force --name "iox-dev-vm-vmware" builds/vmware-ubuntu1404.box
+vagrant box add --force --name "iox-dev-vm-vmware" builds/vmware-ubuntu1404.box; \
+rm -rf builds/*.box
--- iox-dev-vm/scripts/ansible.sh
+++ iox-dev-vm/scripts/ansible.sh
@@ -9,6 +9,16 @@ apt-add-repository ppa:ansible/ansible
 apt-get -y update
 apt-get -y install ansible git
 
+#Install a key for VMware repo
+wget https://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
+apt-key add VMWARE-PACKAGING-GPG-RSA-KEY.pub
+wget https://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub
+apt-key add VMWARE-PACKAGING-GPG-DSA-KEY.pub
+
+#Add VMware repo
+apt-add-repository 'deb http://packages.vmware.com/packages/ubuntu precise main'
+apt-get -y update
+
 cat > /tmp/requirements.yml << EOM
 - name: ansible-role-packer-debian
   src: https://github.com/DevOps4Networks/ansible-role-packer-debian
