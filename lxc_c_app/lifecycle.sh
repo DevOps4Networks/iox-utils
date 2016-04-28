f70313302edb2304f7d3249629223b7aeb7f4bfb
Nathan Sowatskey <nathan@nathan.to>
2016-04-28 09:44:55 -0700

    Getting there ...

---
+++ lxc_c_app/lifecycle.sh
@@ -0,0 +1,7 @@
+ioxclient app stop lxc_c_app
+ioxclient app deactivate lxc_c_app 
+ioxclient app uninstall lxc_c_app 
+ioxclient app install lxc_c_app out/lxc_c_app_ir800-lxc.tar 
+ioxclient app activate lxc_c_app 
+ioxclient app start lxc_c_app 
+ioxclient application console lxc_c_app
